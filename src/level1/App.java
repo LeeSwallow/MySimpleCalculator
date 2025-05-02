34f546e1a71798305d51d71d764862f9c0fdc568
SwallowLee <lmh2157@outlook.kr>
2025-05-02 17:45:52 +0900

    level1 디버깅 완료
    1. Math 클래스를 활용하여 가능한 논리 오류 영역에 대한 처리완료
    
    2. Scanner에서 일어날 수 있는 문제 해결

--- src/level1/App.java
+++ src/level1/App.java
@@ -10,32 +10,45 @@ public class App {
         while (isContinue) {
             try {
                 System.out.print("첫 번째 숫자를 입력하세요:");
+
+                if (!sc.hasNextInt()) {
+                    sc.nextLine();
+                    throw new RuntimeException("제대로 된 형식의 숫자를 입력해 주세요!");
+                }
                 int num1 = sc.nextInt();
+                if (num1 <= 0) throw new RuntimeException("양의 정수를 입력해 주세요!");
+
                 System.out.print("두 번째 숫자를 입력하세요:");
+
+                if (!sc.hasNextInt()) {
+                    sc.nextLine();
+                    throw new RuntimeException("제대로 된 형식의 숫자를 입력해 주세요!");
+                }
                 int num2 = sc.nextInt();
+                if (num2 <= 0) throw new RuntimeException("양의 정수를 입력해 주세요!");
 
                 System.out.print("사칙연산 기호를 입력하세요: ");
                 char operator = sc.next().charAt(0);
 
                 int result = switch (operator) {
-                    case '+' -> num1 + num2;
-                    case '-' -> num1 - num2;
-                    case '*' -> num1 * num2;
-                    case '/' -> num1 / num2;
+                    case '+' -> Math.addExact(num1, num2);
+                    case '-' -> Math.subtractExact(num1, num2);
+                    case '*' -> Math.multiplyExact(num1, num2);
+                    case '/' -> Math.divideExact(num1, num2);
                     default -> throw new RuntimeException("지원하지 않는 연산자 입니다.");
                 };
+
                 System.out.println("결과: " + result);
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                String lastAnswer = sc.next();
-                /* exit을 입력 받으면 반복 종료 */
-                if (lastAnswer.equals("exit")) {
-                    isContinue = false;
-                }
             } catch(Exception e) {
                 System.out.println("에러발생 : " + e.getMessage());
             }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String lastAnswer = sc.next().trim().toLowerCase();
+            /* exit을 입력 받으면 반복 종료 */
+            if (lastAnswer.equals("exit")) {
+                isContinue = false;
+            }
         }
-
-
     }
 }
